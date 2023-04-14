f91881965d4b48a3eff0044da506fde6f140ae40
HungFai Koo <jhk888@yahoo.com>
2023-04-14 16:47:03 -0700

    another commit

---
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress"/>
+
+describe("Test Contact Us from via Automation Test Store", () => {
+  it("Should be able to submit a successful submission via contact us form", () => {
+    // cypress code
+    cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+    cy.get("#ContactUsFrm_first_name").type("Joe");
+    cy.get("#ContactUsFrm_email").type("123@mail.com");
+    cy.get("#ContactUsFrm_enquiry").type("Do you provide additonal discount?");
+    cy.get(".col-md-6 > .btn").click();
+    cy.get("up");
+  });
+});
---
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress"/>
+
+describe("Test Contact Us form via WebdriverUni", () => {
+  it("Should be able to submit a successful submission via contact us form", () => {
+    // cypress code
+    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    // cy.get("#contact-us > .thumbnail").click();
+    cy.get('[name="first_name"]').type("Jacky");
+    cy.get('[name="last_name"]').type("Koo");
+    cy.get('[name="email"]').type("123@mail.com");
+    cy.get("textarea.feedback-input").type("I need to get better for Cypress");
+    cy.get('[type="submit"]').click();
+  });
+
+  it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    // cy.get("#contact-us > .thumbnail").click();
+    cy.get('[name="first_name"]').type("Tom");
+    cy.get('[name="last_name"]').type("Tom");
+    cy.get("textarea.feedback-input").type("How can I learn Cypress");
+    cy.get('[type="submit"]').click();
+  });
+});
